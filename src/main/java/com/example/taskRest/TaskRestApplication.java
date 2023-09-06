c911448524d409a5f90275821b045280d526b8f2
Esteban Torres <estebanto92@gmail.com>
2023-09-06 19:18:58 +0200

    First Commit

---
+++ src/main/java/com/example/taskRest/TaskRestApplication.java
@@ -0,0 +1,13 @@
+package com.example.taskRest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaskRestApplication.class, args);
+	}
+
+}
